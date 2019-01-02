1fd16635a663c5b86d6fe1b52dcd327d2d75de75
Karthick Raj <46133222+karthick120197@users.noreply.github.com>
2019-01-02 18:32:40 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -4,7 +4,7 @@ insert into Bus_info(Bus_no, Bus_name, Bus_type, Bus_capacity, Travels_id) value
 ('1004', 'KPP', 'AC', '35', '3004');
 
 insert into Location(Area_id, Area, City_id, State_id) values('2001', 'Tambaram', '4001', '101'),
-('2002', 'velachery', '4002', 101'),
+('2002', 'velachery', '4002', '101'),
 ('2003', 'guindy', '4003', '101'),
 ('2004', 'guindy', '4004', '101');
 
@@ -28,4 +28,12 @@ insert into Travels(Travels_id, Travels_name) values('3001', 'RRT'),
  ('1003', '400'),
  ('1004', '500');
  
- insert into 
+ insert into Trip(Trip_id, Bus_no, From, To, Capacity, Seat_available) values('5001', '1001', 'chennai', 'trichy', '35', '12'),
+ ('5002', '1002', 'chennai', 'madurai', '35', '05'),
+ ('5003', '1003', 'chennai', 'theni', '35', '03'),
+ ('5004', '1004', 'chennai', 'madurai', '35', '00');
+ 
+ insert into Booking(Booking_id, Trip_id, Customer_name, mode_of_payment) values('6001', '5001', 'SSS', 'cash'),
+ ('6002', '5002', 'AAA', 'card'),
+ ('6003', '5003', 'XXX', 'card'),
+ ('6004', '5004', 'YYY', 'card');
