b6052cf5aac5437c796f9af2d369ca4c3192d373
Karthick Raj <46133222+karthick120197@users.noreply.github.com>
2019-01-24 16:10:45 +0530

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -5,5 +5,5 @@ create table Travels(Travels_id int not null, Travels_name varchar(100) not null
 create table City(State_id int not null, City_id int not null, City_name vrachar(50) not null);
 create table State(State_id int not null, State_name varchar(50) not null);
 create table Rates(Bus_no int not null, Rate int not null);
-create table Trip(Trip_id int not null, Bus_no int not null, From varchar(50) int not null, To varchar(50) int not null, Capacity int not null, Seat_available int not null, Time sysdatetime());
+create table Trip(Trip_id int not null, Bus_no int not null, From_area varchar(50) not null, To_area varchar(50) not null, Capacity int not null, Seat_available int not null, bus_time date default getdate();
 create table Booking( Booking_id int not null, Trip_id int not null, Customer_name varchar(50) not null, Mode_of_payment varchar(50) not null);
