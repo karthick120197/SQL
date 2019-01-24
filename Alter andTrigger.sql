9cf924a81f433a84bdd6313996facb61a1fbe502
Karthick Raj <46133222+karthick120197@users.noreply.github.com>
2019-01-24 16:27:18 +0530

    Update Alter andTrigger.sql

--- Alter andTrigger.sql
+++ Alter andTrigger.sql
@@ -2,12 +2,15 @@ alter table Bus_info add primary key (Bus_no);
 alter table trip add foreign key (Bus_no) references bus_info(Bus_no);
 select count(Bus_name), Bus_type from bus_info group by Bus_type; 
 select * from bus_info inner join trip on bus_info.Bus_no=trip.Bus_no;
+select avg(Rate) from Rates where Bus_no=1004;
+
 # STROED PROCEDURES
 
-create procedure avg_rate 
+create procedure max_rate 
 as 
 Begin 
-    select count(Bus_name), Bus_type from bus_info group by Bus_type;
+    select * from Bus_info inner join Rates on Bus_info.Bus_no=Rates.Bus_no where (
+    select Bus_no, Rate from Rates having max(Rate));
 END; 
 
 #TRIGGER
@@ -17,9 +20,11 @@ CREATE TRIGGER updatetrigger
     AFTER UPDATE ON Bus_info
     FOR EACH ROW 
 BEGIN
-    INSERT INTO trip
+    INSERT INTO Booking
     SET action = 'update',
-     Seat_available = Seat_available-1,
-        lastname = OLD.lastname; 
+     Booking_id = NEW.Booking_id,
+        Trip_id = NEW.Trip_id,
+        Customer_name=NEW.Customer_name,
+        Mode_of_payment=NEW.Mode_of_payment; 
 END$$
 DELIMITER ;
