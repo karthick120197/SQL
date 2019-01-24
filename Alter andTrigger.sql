76f99e476974489c0c4024fd4f07fb5a5362de7c
Karthick Raj <46133222+karthick120197@users.noreply.github.com>
2019-01-24 16:03:42 +0530

    Update Alter andTrigger.sql

--- Alter andTrigger.sql
+++ Alter andTrigger.sql
@@ -1,8 +1,14 @@
 alter table Bus_info add primary key (Bus_no);
-alter table trip add foregin key (Bus_no);
+alter table trip add foreign key (Bus_no) references bus_info(Bus_no);
 select count(Bus_name), Bus_type from bus_info group by Bus_type; 
 select * from bus_info inner join trip on bus_info.Bus_no=trip.Bus_no;
-alter table trip add foreign key (Bus_no) references bus_info(Bus_no);
+# STROED PROCEDURES
+
+create procedure avg_rate 
+as 
+Begin 
+    select count(Bus_name), Bus_type from bus_info group by Bus_type;
+END; 
 
 #TRIGGER
 
