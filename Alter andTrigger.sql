fb717e05e9a62bad954d9c4a1eab00e00a3c225f
Karthick Raj <46133222+karthick120197@users.noreply.github.com>
2019-01-29 11:16:39 -0600

    Update Alter andTrigger.sql

--- Alter andTrigger.sql
+++ Alter andTrigger.sql
@@ -1,11 +1,4 @@
-alter table Bus_info add primary key (Bus_no);
-alter table trip add foreign key (Bus_no) references bus_info(Bus_no);
-select count(Bus_name), Bus_type from bus_info group by Bus_type; 
-select * from bus_info inner join trip on bus_info.Bus_no=trip.Bus_no;
-select avg(Rate) from Rates where Bus_no=1004;
-
-# STROED PROCEDURES
-
+# STORED PROCEDURES
 create procedure max_rate 
 as 
 Begin 
@@ -14,7 +7,6 @@ Begin
 END; 
 
 #TRIGGER
-
 DELIMITER $$
 CREATE TRIGGER updatetrigger 
     AFTER UPDATE ON Bus_info
